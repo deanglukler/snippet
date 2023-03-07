bfbeb9fb023edbe0a59b511d4d586f30f902e1ad
Dean Glueckler <glueckler@gmail.com>
2023-03-07 12:25:46 -0800

    added bugsnag

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -2,6 +2,21 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Bugsnag from '@bugsnag/js';
+import BugsnagPluginReact from '@bugsnag/plugin-react';
+import React from 'react';
+
+Bugsnag.start({
+  apiKey: '6cd4815af1900f7dc947cecfaf8e965f',
+  plugins: [new BugsnagPluginReact()],
+});
+
+const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React);
+
 const container = document.getElementById('root')!;
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
+);
