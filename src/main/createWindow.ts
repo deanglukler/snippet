aa03a8987c70151ed7eecb6e8d7bfc10a1aeed6d
Dean Glueckler <glueckler@gmail.com>
2023-08-19 15:23:16 -0700

    remove react dev tools

--- src/main/createWindow.ts
+++ src/main/createWindow.ts
@@ -9,11 +9,12 @@ const createWindow: () => Promise<BrowserWindow> = async () => {
     // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
     const installer = require('electron-devtools-installer');
     const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-    const extensions = ['REACT_DEVELOPER_TOOLS'];
 
     return installer
       .default(
-        extensions.map((name) => installer[name]),
+        [
+          /* 'REACT_DEVELOPER_TOOLS' */
+        ].map((name) => installer[name]),
         forceDownload
       )
       .catch(log);
