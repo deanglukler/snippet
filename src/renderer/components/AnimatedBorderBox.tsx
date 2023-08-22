3a66fdbb42af716e57a4c44a1b60abf75500df13
Dean Glueckler <glueckler@gmail.com>
2023-08-21 23:32:41 -0700

    new snippet border color fix

--- src/renderer/components/AnimatedBorderBox.tsx
+++ src/renderer/components/AnimatedBorderBox.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { useTheme } from '../hooks';
 
 const AnimatedBorderBox: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const theme = useTheme();
+
+  const animationBgColor = theme.token.colorPrimary;
+
   return (
     <div className="box-outer">
-      <div className="main-box list-card">
-        <div className="bar top" />
-        <div className="bar right delay" />
-        <div className="bar bottom delay" />
-        <div className="bar left" />
+      <div
+        className="main-box list-card"
+        style={{ borderColor: theme.token.colorBgContainer }}
+      >
+        <div className="bar top" style={{ background: animationBgColor }} />
+        <div
+          className="bar right delay"
+          style={{ background: animationBgColor }}
+        />
+        <div
+          className="bar bottom delay"
+          style={{ background: animationBgColor }}
+        />
+        <div className="bar left" style={{ background: animationBgColor }} />
         {children}
       </div>
     </div>
